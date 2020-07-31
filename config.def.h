cda642be167212fcbc72fd0eff18d040792420c1
tdu <tdukv@protonmail.com>
2020-07-31 22:51:13 +0300

    rules for xmessage

--- config.def.h
+++ config.def.h
@@ -73,6 +73,7 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,     NULL,   1 << 8,         0,          0,         -1,        -1 },
 	{ "st",       NULL,     NULL,        0,         0,          1,         -1,        -1 },
 	{ "St",       NULL,     NULL,        0,         0,          1,         -1,        -1 },
+	{ "Xmessage", NULL,     NULL,        0,         1,          0,          1,        -1 },
 	{ NULL,       NULL,  "Event Tester", 0,         1,          0,          1,        -1 }, /* xev */
 };
 
