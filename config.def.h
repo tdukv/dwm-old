c2137fa1f2c33da319d8fed738427f91d3143499
tdu <tdukv@protonmail.com>
2020-09-12 13:22:39 +0300

    save floats patch

--- config.def.h
+++ config.def.h
@@ -74,6 +74,7 @@ static const Rule rules[] = {
 	{ "st",       NULL,     NULL,        0,         0,          1,         -1,        -1 },
 	{ "St",       NULL,     NULL,        0,         0,          1,         -1,        -1 },
 	{ "Xmessage", NULL,     NULL,        0,         1,          0,          1,        -1 },
+	{ "XCalc",    NULL,     NULL,        0,         1,          0,          0,        -1 },
 	{ NULL,       NULL,  "Event Tester", 0,         1,          0,          1,        -1 }, /* xev */
 };
 
