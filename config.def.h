2ef48e08e9451c26f7aa2833a9f6431571e359b8
tdu <tdukv@protonmail.com>
2020-09-23 20:56:55 +0300

    rule for floating terminal

--- config.def.h
+++ config.def.h
@@ -69,6 +69,7 @@ static const Rule rules[] = {
 	/* class     instance  title   tag mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",     NULL,     NULL,        0,         1,          0,          0,        -1 },
 	{ "fehfloat", NULL,     NULL,        0,         1,          0,          1,        -1 },
+	{ "trmfloat", NULL,     NULL,        0,         1,          0,          1,        -1 },
 	{ "Emacs",    NULL,     NULL,        0,         0,          0,          1,        -1 },
 	{ "Firefox",  NULL,     NULL,   1 << 8,         0,          0,         -1,        -1 },
 	{ "st",       NULL,     NULL,        0,         0,          1,         -1,        -1 },
