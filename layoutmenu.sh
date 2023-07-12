0d39be7b8f4d785fe46cfca60fc17fe6eb8e19e0
tdu <tdukv@protonmail.com>
2023-07-13 00:45:56 +0300

    update layoutmenu

--- layoutmenu.sh
+++ layoutmenu.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 cat <<EOF | xmenu
-[]= Right Stack Layout	0
-TTT Bottom Stack Layout	1
-|M| Centered Maser Layout	2
->M> centeredfloatingmaster	3
-><> Floating Layout	5
-[M] Monocle Layout	6
+[]= Tile	0
+[@] Spiral	1
+[\] Dwindle	2
+H[] Deck	3
+TTT Bottom Stack	4
+HHH Grid	5
+### N Row Grid	6
+--- Horizontal Grid	7
+::: Gapless Grid	8
+|M| Dentered Master	9
+
+><> Floating	11
+[M] monocle	12
 EOF
